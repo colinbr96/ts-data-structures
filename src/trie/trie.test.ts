562f6bb87f30921545634ed3ca532078d955b696
Colin Brown <kolivbrauv@gmail.com>
2022-10-08 21:14:06 -0700

    Add unit tests for searching Tries

--- src/trie/trie.test.ts
+++ src/trie/trie.test.ts
@@ -19,4 +19,45 @@ describe("Trie", () => {
     expect(trie.root.children.a?.children.b?.children.c?.children.d?.val).toBe("d");
     expect(trie.root.children.a?.children.b?.children.c?.children.d?.end).toBe("abcd");
   });
+
+  describe("search", () => {
+    let trie = new Trie();
+
+    beforeAll(() => {
+      trie.insert("tea");
+      trie.insert("ted");
+      trie.insert("ten");
+      trie.insert("inn");
+    });
+
+    it("finds an existing word", () => {
+      expect(trie.search("tea")).toBe(true);
+    });
+
+    it("doesn't find a non-existent word", () => {
+      expect(trie.search("te")).toBe(false);
+      expect(trie.search("tep")).toBe(false);
+      expect(trie.search("teas")).toBe(false);
+    });
+  });
+
+  describe("startsWith", () => {
+    let trie = new Trie();
+
+    beforeAll(() => {
+      trie.insert("tea");
+      trie.insert("ted");
+      trie.insert("ten");
+      trie.insert("inn");
+    });
+
+    it("finds an existing prefix", () => {
+      expect(trie.startsWith("te")).toBe(true);
+      expect(trie.startsWith("tea")).toBe(true);
+    });
+
+    it("doesn't find a non-existent prefix", () => {
+      expect(trie.startsWith("tep")).toBe(false);
+    });
+  });
 });
